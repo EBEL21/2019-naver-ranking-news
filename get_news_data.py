0969eb5b82dc0e230b7cfbf8f9708116539b2448
Taeho Kang <30804387+EBEL21@users.noreply.github.com>
2020-01-23 16:00:42 +0900

    Add files via upload
    - 네이버가 기사를 삭제했을 때 데이터를 가져올 수 없었던 현상 수정
    - age, reaction 관련 데이터 가져올 때 버그 수정

--- get_news_data.py
+++ get_news_data.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException, NoSuchAttributeException
+from selenium.common.exceptions import NoSuchElementException, NoSuchAttributeException, TimeoutException
 import urllib.parse
 import urllib.request
 import requests
@@ -30,7 +30,11 @@ def date_range(start_date, end_date):
 # 각 섹션 별 최근까지 저장한 날짜를 탐색
 def find_recently_date(section_id):
     data = collection.find_one({'section': section_id}, sort=[('month', -1), ('day', -1)])
-    return [data['month'], data['day']]
+    day, month = {data['month'], data['day']}
+    rd_count = collection.find({'section': section_id, 'month': data['month'], 'day': data['day']}).count()
+    if rd_count != 30:
+        collection.delete_many({'section': section_id, 'month': data['month'], 'day': data['day']})
+    return [day, month]
 
 
 def create_url(_date, _section_id):
@@ -54,7 +58,7 @@ def article_threading(section_id):
     다음 두 줄 주석 해제
     '''
     r_date = find_recently_date(section_id)
-    start_date = date(2019, r_date[0], r_date[1]) + timedelta(days=1)
+    start_date = date(2019, r_date[0], r_date[1])
 
     for single_date in date_range(start_date, end_date):
         url = create_url(single_date, section_id)
@@ -82,6 +86,7 @@ def get_article_base_info(year, month, day, section_id, url, driver):
         info["year"] = year
         info["month"] = month
         info["day"] = day
+        print(info)
         collection.insert_one(info)
 
 
@@ -90,19 +95,27 @@ def get_article_inner_info(article_url, driver):
     info = {}
 
     driver.get(article_url)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'u_likeit_layer')))
-    article_html = driver.page_source
-    a_soup = BeautifulSoup(article_html, 'html.parser')
+    try:
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, '_reactionModule')))
+        article_html = driver.page_source
+        a_soup = BeautifulSoup(article_html, 'html.parser')
 
-    # viewer reaction
-    # reaction 순서 좋아요/훈훈해요/슬퍼요/화나요/후속기사 원해요
-    a_reaction = a_soup.find("ul", attrs={"class": "u_likeit_layer"})  # reaction panel
-    reactions = a_reaction.find_all("span", attrs={"class": "u_likeit_list_count"})
-    info["reaction_good"] = value_change(reactions[0].text)
-    info["reaction_warm"] = value_change(reactions[1].text)
-    info["reaction_sad"] = value_change(reactions[2].text)
-    info["reaction_angry"] = value_change(reactions[3].text)
-    info["reaction_next"] = value_change(reactions[4].text)
+        # viewer reaction
+        # reaction 순서 좋아요/훈훈해요/슬퍼요/화나요/후속기사 원해요
+        a_reaction = a_soup.find("ul", attrs={"class": "u_likeit_layer"})  # reaction panel
+        reactions = a_reaction.find_all("span", attrs={"class": "u_likeit_list_count"})
+        info["reaction_good"] = value_change(reactions[0].text)
+        info["reaction_warm"] = value_change(reactions[1].text)
+        info["reaction_sad"] = value_change(reactions[2].text)
+        info["reaction_angry"] = value_change(reactions[3].text)
+        info["reaction_next"] = value_change(reactions[4].text)
+    except TimeoutException:
+        try:
+            error = driver.find_element_by_class_name('error')
+        except NoSuchElementException:
+            pass
+        else:
+            return info
 
     # move to comment window
     try:
@@ -110,16 +123,16 @@ def get_article_inner_info(article_url, driver):
     except NoSuchElementException:
         pass
     else:
-        if go_to_comment != None:
-            go_to_comment.click()
+        go_to_comment.click()
     finally:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'cbox_module')))
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'cbox_module')))
         article_html = driver.page_source
         a_soup = BeautifulSoup(article_html, 'html.parser')
 
     # 총 댓글 수
     try:
         a_comment = a_soup.find("span", attrs={"class": "u_cbox_count"}).text  # comment
+        print(a_comment)
     except AttributeError:
         info["comment"] = -1
     else:
@@ -160,6 +173,7 @@ def get_article_inner_info(article_url, driver):
     return info
 
 
+# your mongodb host
 client = MongoClient('localhost', 27017)
 db = client["news"]
 collection = db["article"]
@@ -169,9 +183,9 @@ base_news_url = "https://news.naver.com"
 news_ranking_url = base_news_url + "/main/ranking/popularDay.nhn?rankingType=popular_day"
 
 chromedriver_path = r'B:\바탕화면\chromedriver.exe'
-# driver = webdriver.Chrome(chromedriver_path)
+driver = webdriver.Chrome(chromedriver_path)
 # date parameters
-start_date = date(2019, 8, 8)
+start_date = date(2019, 10, 15)
 end_date = date(2019, 12, 31) + timedelta(days=1)
 
 if __name__ == '__main__':
@@ -184,9 +198,13 @@ if __name__ == '__main__':
     for i in range(100, 106, 2):
         mp.Process(target=article_processing, args=(i,)).start()
     '''
-    start_time = time.time()
-    with cf.ProcessPoolExecutor() as executor:
-        for msg in executor.map(article_processing, range(100, 106, 2)):
-            print(msg)
-    duration = time.time() - start_time
-    print(duration)
+    # start_time = time.time()
+    # with cf.ProcessPoolExecutor() as executor:
+    #     for msg in executor.map(article_processing, range(100, 106, 2)):
+    #         print(msg)
+    # duration = time.time() - start_time
+    # print(duration)
+    section_id = 105
+    for single_date in date_range(start_date, end_date):
+        url = create_url(single_date, section_id)
+        get_article_base_info(single_date.year, single_date.month, single_date.day, section_id, url, driver)
